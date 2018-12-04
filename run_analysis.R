7f8d62fec90c33e4d248140448551603a8cb5568
scottberta <43867091+scottberta@users.noreply.github.com>
2018-12-03 19:29:48 -0500

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 # run_analysis.R
 # Data collected from Samsung Galaxy S II mobile phone is
-# cleaned and analysed. Resulting data is output to a tidy
+# cleaned and analysed. Resulting data is oputput to a tidy
 # data file named "tidy_data.txt"
 
 library(dplyr)
@@ -23,42 +23,62 @@ if(!file.exists(zipfilename))
 
 unzip(zipfilename, exdir = datadir)
 
-# Read test data and cbind to create one training data set
+# Read Subject data and combine to one Subject data set
 trainSubject<-read.table(file.path(datadir,datapath,"train/subject_train.txt"))
-trainLabels<-read.table(file.path(datadir,datapath,"train/y_train.txt"))
-trainValues<-read.table(file.path(datadir,datapath,"train/X_train.txt"))
-trainData<-cbind(trainSubject, trainLabels, trainValues)
-
-# Read test data and cbind to create one test data set
 testSubject<-read.table(file.path(datadir,datapath,"test/subject_test.txt"))
-testLabels<-read.table(file.path(datadir,datapath,"test/y_test.txt"))
-testValues<-read.table(file.path(datadir,datapath,"test/X_test.txt"))
-testData<-cbind(testSubject, testLabels, testValues)
+subjectData <- rbind(trainSubject, testSubject)
 
-#Merge testData and trainData with rbind to create allData
-allData<-rbind(trainData, testData)
+# Read Labels and combine to create one data set
+trainLabels<-read.table(file.path(datadir,datapath,"train/y_train.txt"))
+testLabels<-read.table(file.path(datadir,datapath,"test/y_test.txt"))
+labelData<- rbind(trainLabels, testLabels)
+
+# Read features data and combine to create one data set
+trainFeatures <-read.table(file.path(datadir,datapath,"train/X_train.txt"))
+testFeatures<-read.table(file.path(datadir,datapath,"test/X_test.txt"))
+featuresData <- rbind(trainFeatures,testFeatures)
+
+# Rename column names in featuresData with names from features.txt
+features<-read.table(file.path(datadir,datapath,"features.txt"), as.is = TRUE)
+colnames(featuresData)<-t(features[2])
+
+#Merge subject, label and feature data with cbind to create allData
+colnames(subjectData)<-"Subject"
+colnames(labelData)<-"Activity"
+allData<-cbind(subjectData, labelData, featuresData)
 
 #Read activity names and update column names
 activityLabels<-read.table(file.path(datadir,datapath,"activity_labels.txt"))
 colnames(activityLabels) <- c("activityId", "activityLabel")
 
-#Read in features and extract wanted measure (Mean, STD)
-features<-read.table(file.path(datadir,datapath,"features.txt"), as.is = TRUE)
-wantedMeasures<-grep(".*mean.*|.*std.*", features[,2])
-wantedMeasures.names <- features[wantedMeasures,2]
-wantedMeasures.names = gsub('-mean', 'Mean', wantedMeasures.names)
-wantedMeasures.names = gsub('-std', 'Std', wantedMeasures.names)
-wantedMeasures.names = gsub('[-()]','',wantedMeasures.names)
+# Determine which columns to keep (containing Mean and Std)
+wantedFeatures <-grep(".*mean.*|.*std.*", names(allData))
+keepColumns <- c(1,2,wantedFeatures)
 
-#Add column names to allData
-colnames(allData) <- c("subject", "activity", wantedMeasures.names)
+# Extract columns to keep from allData
+allDataMeans <- allData[,keepColumns]
+
+# Apply descriptive variable names to allDataMeans
+
+names(allDataMeans)<-gsub("Acc", "Accelerometer", names(allDataMeans))
+names(allDataMeans)<-gsub("Gyro", "Gyroscope", names(allDataMeans))
+names(allDataMeans)<-gsub("BodyBody", "Body", names(allDataMeans))
+names(allDataMeans)<-gsub("Mag", "Magnitude", names(allDataMeans))
+names(allDataMeans)<-gsub("^t", "Time", names(allDataMeans))
+names(allDataMeans)<-gsub("^f", "Frequency", names(allDataMeans))
+names(allDataMeans)<-gsub("tBody", "TimeBody", names(allDataMeans))
+names(allDataMeans)<-gsub("-mean()", "Mean", names(allDataMeans), ignore.case = TRUE)
+names(allDataMeans)<-gsub("-std()", "STD", names(allDataMeans), ignore.case = TRUE)
+names(allDataMeans)<-gsub("-freq()", "Frequency", names(allDataMeans), ignore.case = TRUE)
+names(allDataMeans)<-gsub("angle", "Angle", names(allDataMeans))
+names(allDataMeans)<-gsub("gravity", "Gravity", names(allDataMeans))
 
 #Turn activities and subjects into factors
-allData$activity <- factor(allData$activity, levels=activityLabels[, 1], labels=activityLabels[, 2])
-allData$subject <- as.factor(allData$subject)
+allDataMeans$Activity <- factor(allDataMeans$Activity, levels=activityLabels[, 1], labels=activityLabels[, 2])
+allDataMeans$Subject <- as.factor(allDataMeans$Subject)
 
 #Group by activity and subject and calculate mean of each measure
-allDataMeans <- aggregate(. ~subject + activity, allData, mean)
+allDataMeansOutput <- aggregate(. ~Subject + Activity, allDataMeans, mean)
 
 #Write output to text file "tidy_data.txt"
-write.table(allDataMeans,file="tidy_data.txt",row.names=FALSE)
+write.table(allDataMeansOutput,file="tidy_data.txt",row.names=FALSE)
